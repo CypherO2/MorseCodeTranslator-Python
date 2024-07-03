499ffa8ade79d00c3a6d95277401f8ac2d6e2096
Cassi <120105652+CypherO2@users.noreply.github.com>
2024-07-03 09:37:20 +0000

    morse key reads from file

--- main.py
+++ main.py
@@ -1,57 +1,23 @@
-MorseKey = {
-    "A": ".-",
-    "B": "-...",
-    "C": "-.-.",
-    "D": "-..",
-    "E": ".",
-    "F": "..-.",
-    "G": "--.",
-    "H": "....",
-    "I": "..",
-    "J": ".---",
-    "K": "-.-",
-    "L": ".-..",
-    "M": "--",
-    "N": "-.",
-    "O": "---",
-    "P": ".--.",
-    "Q": "--.-",
-    "R": ".-.",
-    "S": "...",
-    "T": "-",
-    "U": "..-",
-    "V": "...-",
-    "W": ".--",
-    "X": "-..-",
-    "Y": "-.--",
-    "Z": "--..",
-    "1": ".----",
-    "2": "..---",
-    "3": "...--",
-    "4": "....-",
-    "5": ".....",
-    "6": "-....",
-    "7": "--...",
-    "8": "---..",
-    "9": "----.",
-    "0": "-----",
-    ", ": "--..--",
-    ".": ".-.-.-",
-    "?": "..--..",
-    "/": "-..-.",
-    "-": "-....-",
-    "(": "-.--.",
-    ")": "-.--.-",
-}
+morseKey = {}
+
+with open("morsecodeKey.txt","r") as morseKey_RAW:
+    morseKey_RAW = morseKey_RAW.read()
+    morseKey_RAW = morseKey_RAW.split("~")
+    for KeyVal in morseKey_RAW:
+        pair = KeyVal.split(":")
+        key, val = pair[0], pair[1]
+        morseKey[key] = val
+
+# print(morseKey)
 
 testCode1 = "-.--.-"
 testCode2 = "test."
 
 
 # Initial Function Declaring User's Choice
-def main():
+def main() -> None:
     menuChoice = input(
-        "What would you link to do?\n1. Encrypt Text\n2. Decrypt Code\n> > > "
+        "What would you like to do?\n1. Encrypt Text\n2. Decrypt Code\n> > > "
     )
     while int(menuChoice) not in range(1, 3):
         menuChoice = input(
@@ -63,9 +29,8 @@ def main():
         Decryption()
     else:
         raise Exception(
-            "Something went wrong during the initial input sequence!\n Line 49 -> 65"
+            "Something went wrong during the initial input sequence!\n Line 25 -> 41"
         )
-    return
 
 
 # Function that handles the encryption of text
@@ -74,6 +39,7 @@ def Encryption():
     userText = input("What would you like to encrypt?\n> > > ")
     while type(userText) == str and userText != "":
         userText = input("What would you like to encrypt?\n> > > ")
+    
     return
 
 
@@ -84,14 +50,23 @@ def MorseValidation(morseCode):
             return "Code Morsing"
         else:
             return "Not Very Morse"
+
+    if morseCode in morseKey.values():
+        return "Morse Code is Very Morse"
+    elif morseCode not in morseKey.values():
+        return "Morse Not Code"   
+    
+
     return
 
 
 # Function that handles the decryption of morse code
 def Decryption():
-    # print("Decryption") - Test Complete
-    userCode = testCode2  # input("What would you like to decrypt?\n> > > ")
-    print(MorseValidation(userCode))  # Validation Function
+    # # print("Decryption") - Test Complete
+    # userCode = testCode2  # input("What would you like to decrypt?\n> > > ")
+    # print(MorseValidation(userCode))  # Validation Function
+    print("Not Yet Implemented")
+    main()
     return
 
 
